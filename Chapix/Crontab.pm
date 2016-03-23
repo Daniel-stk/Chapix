4db7a910b8e307b2fe7e92075e990a870d6c8e85
David Romero <romdav@xaandia.com>
2016-03-22 21:47:45 -0400

    Crons

--- Chapix/Crontab.pm
+++ Chapix/Crontab.pm
@@ -8,7 +8,36 @@ use Carp;
 use Chapix::Conf;
 use Chapix::Com;
 
-sub run {
+sub run_minute {
+    my $DEBUG = shift;
+    print time() . " Chapix Cron RUN \n" if ($DEBUG);
+    print time() . "\n" if ($DEBUG);
+    print time() . "\n" if ($DEBUG);
+    my $modules = $dbh->selectcol_arrayref("SELECT m.module FROM xaa.modules m WHERE m.installed=1");
+    foreach my $module (@$modules){
+        print time() . " Module $module\n" if ($DEBUG);
+        # Load module
+        my $Module;
+        eval {
+            require "Chapix/" . $module ."/Crontab.pm";
+            my $module_name ='Chapix::'.$module.'::Crontab';
+            $Module = $module_name->new($DEBUG);
+        };
+        if($@){
+            print time() . " Module $module error $@\n" if ($DEBUG);
+            next;
+        }
+
+        # Actions
+        $Module->run_minute();
+        
+        print time() . " Module $module END\n" if ($DEBUG);
+        print time() . "\n" if ($DEBUG);
+        print time() . "\n" if ($DEBUG);
+    }
+}
+
+sub run_daily {
     my $DEBUG = shift;
     print time() . " Chapix Cron RUN \n" if ($DEBUG);
     print time() . "\n" if ($DEBUG);
@@ -29,7 +58,7 @@ sub run {
         }
 
         # Actions
-        $Module->run();
+        $Module->run_daily();
         
         print time() . " Module $module END\n" if ($DEBUG);
         print time() . "\n" if ($DEBUG);
--- cron_minute.pl
+++ cron_minute.pl
@@ -2,15 +2,15 @@
 
 use strict;
 use lib ('/var/www/html');
+use lib ('/usr/local/WS/Chapix');
 use CGI qw/:cgi/;
 use CGI::Carp qw(fatalsToBrowser);
-
-
+chdir ('/usr/local/WS/Chapix/');
 use Chapix::Conf;
 use Chapix::Com;
 use Chapix::Crontab;
 
 my $DEBUG = 1;
-Chapix::Crontab::run($DEBUG);
+Chapix::Crontab::run_minute($DEBUG);
 Chapix::Com::app_end();
 exit;
