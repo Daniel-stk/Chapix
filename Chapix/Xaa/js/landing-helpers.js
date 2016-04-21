443db263bec18083415c586114caf9a7906454a9
Cesar Rodriguez <cesarrodriguez@xaandia.com>
2016-04-21 18:30:52 -0400

    Help page save referrer on forms spanglish

--- Chapix/Xaa/js/landing-helpers.js
+++ Chapix/Xaa/js/landing-helpers.js
@@ -27,35 +27,8 @@ $(function(){
     	}
     });
 
-
-
     $('select').material_select();
            
-	// $("#mkt_country").jeoCountrySelect({
- //        callback: function () {
- //        	$('#mkt_country').removeClass('browser-default');
- //        	$("#mkt_country").material_select();
- //        }
- //    });
-
- //    $(document).on('change', '#mkt_country', function() {
- //    	var countryID = $(this).val();
-        
- //    	if ( $('#mkt_state').length ){
- //            var innerHtml = '';
- //    		jeoquery.getGeoNames('countryInfo', {country : countryID}, function(data){  		
- // 				jeoquery.getGeoNames('children', {geonameId : data.geonames[0].geonameId}, function(states){ 
- // 					$.each(states.geonames, function(key, value){
- // 						innerHtml += '<option value="'+value.name+'">'+value.name+'</option>';
- // 					});
- //                    $('#mkt_state').html(innerHtml);
- //                    $('#mkt_state').removeClass('browser-default');
- //                    $('#mkt_state').material_select(); 
- // 				});
- // 			});
- //    	}
- //    });
-
-
+    $('form').append('<input type="hidden" class="hide" name="referrer" value="'+document.referrer+'"/>');
 
 }); // DOCUMENT READY 
